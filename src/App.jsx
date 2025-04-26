5ba7edc0242e99b369ff3969063f8109c58d6c9f
Tomasz Rymarczyk <tomako0@gmail.com>
2025-04-26 13:54:45 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div>
+            <header>
+                <h1>do zrobienia</h1>
+                <h2>5 zadań</h2>
+                <button>+</button>
+            </header>
+        </div>
+    );
+}
+
+export default App;
